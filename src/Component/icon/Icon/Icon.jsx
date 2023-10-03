9224818fe514125dc61289e335ce92b70ce83425
LAKSHAY BANSAL <119696323+LAKSHAYBANSAL879@users.noreply.github.com>
2023-10-03 20:30:48 +0530

    Add files via upload

---
+++ src/Component/icon/Icon/Icon.jsx
@@ -0,0 +1,15 @@
+
+import {FaRegCircle,FaTimes,FaPen} from 'react-icons/fa';
+import './Icon.css'
+function Icon({name}) {
+    if(name=="circle"){
+        return <FaRegCircle/>
+    }
+    else if(name=="cross"){
+        return <FaTimes/>
+    }
+    else{
+        return <FaPen/>
+    }
+}
+export default Icon
