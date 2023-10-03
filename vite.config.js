606c798cfc480b2d8e7d88e1a9f84eeb309339fc
LAKSHAY BANSAL <119696323+LAKSHAYBANSAL879@users.noreply.github.com>
2023-10-03 20:39:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/TIC-TAC-TOE-REACT-GAME/"
+  base:'/TIC-TAC-TOE-REACT-GAME/'
 })
